34ad9930c8a3e568974bdae14418d29316af738d
AnndyR <anndy.rengifo@epn.edu.ec>
2025-01-26 00:14:08 -0500

    Verifiacion del TEST

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, PoliSalud! :)");
+
+        try {
+            // Llamar a las pruebas directamente
+            // TestApp.testCorreoDAO();
+            // TestApp.testCatalogoTipoDAO();
+            // TestApp.testCentroMedicoDAO();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
