217d91c75e6c1899431fc5f2c2a40d40afc53fd2
joan <joan.bedon@epn.edu.ec>
2025-01-30 22:33:41 -0500

    GUI

--- src/App.java
+++ src/App.java
@@ -1,54 +1,37 @@
-import java.util.Date;
-
-import DataAccess.DAO.CatalogoDAO;
-import DataAccess.DAO.CatalogoTipoDAO;
-import DataAccess.DAO.CentroMedicoDAO;
-import DataAccess.DAO.CitaDAO;
-import DataAccess.DAO.CorreoDAO;
-import DataAccess.DAO.HistorialClinicoDAO;
-import DataAccess.DAO.MedicoDAO;
-import DataAccess.DAO.PacienteDAO;
-import DataAccess.DAO.PersonaDAO;
-import DataAccess.DTO.CatalogoDTO;
-import DataAccess.DTO.CatalogoTipoDTO;
-import DataAccess.DTO.CentroMedicoDTO;
-import DataAccess.DTO.CitaDTO;
-import DataAccess.DTO.CorreoDTO;
-import DataAccess.DTO.HistorialClinicoDTO;
-import DataAccess.DTO.MedicoDTO;
-import DataAccess.DTO.PacienteDTO;
-import DataAccess.DTO.PersonaDTO;
+import UserInterface.Form.MainForm;
+import UserInterface.Form.SplashScreenForm;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, PoliSalud! :)");
+        SplashScreenForm.show();
+        MainForm frmMain = new MainForm("POLISALUD");
 
         //Update y Delete funcionan similar por el borrado lógico
-        CitaDAO dAO = new CitaDAO();
-        CitaDTO dTO = dAO.read(1); //Recupera el objeto catalogo con id 1
+        //CitaDAO dAO = new CitaDAO();
+        //CitaDTO dTO = dAO.read(1); //Recupera el objeto catalogo con id 1
         
         //dTO.setHoraCita("20:00:00");  //Modifica el nombre del catalogo
 
         //dAO.update(dTO); //Actualiza el catalogo con id 1
 
         //Delete
-        dAO.delete(2);
+        //dAO.delete(2);
 
         //Create
         //dAO.create(new CitaDTO(1, 1, "2025-01-18", "17:20:00"));
 
         //Read
-        for (CitaDTO dto : dAO.readAll()) {
-            System.out.println(dto);
-        }
+        //for (CitaDTO dto : dAO.readAll()) {
+            //System.out.println(dto);
+       //}
 
-        try {
+        //try {
             // Llamar a las pruebas directamente
             // TestApp.testCorreoDAO();
             //TestApp.testCatalogoTipoDAO();
             // TestApp.testCentroMedicoDAO();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
+        //} catch (Exception e) {
+        //    e.printStackTrace();
+        //}
+    //}
+}}
---
+++ src/UserInterface/Form/MenuPanel.java
@@ -0,0 +1,42 @@
+package UserInterface.Form;
+
+import UserInterface.CustomerControl.PatButton;
+import UserInterface.IAStyle;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class MenuPanel extends JPanel {
+    public  PatButton   
+            btnHome     = new PatButton("Home"),
+            btnLogin    = new PatButton("Login"),
+            btnCita     = new PatButton("Cita");
+
+    public MenuPanel(){
+        customizeComponent();
+    }
+
+    private void customizeComponent() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setPreferredSize(new Dimension(250, getHeight())); 
+
+        // add-logo
+        try {
+            Image logo = ImageIO.read(IAStyle.URL_LOGO);
+            logo = logo.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+            add(new JLabel(new ImageIcon(logo)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // add-botones
+        add(btnHome);
+        add(btnLogin);
+        add(btnCita);
+    }
+}
